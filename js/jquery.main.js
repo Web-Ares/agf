26ca3802f44925fa54c269c853585fa971d51fbb
Sergey <kuzmenkos1986@gmail.com>
2016-04-11 12:34:25 +0300

    inside page

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -1,29 +1,29 @@
 "use strict";
-( function(){
+( function() {
 
-    $( function(){
+    $( function() {
 
-        $('.catalog').each(function () {
+        $( '.catalog' ).each( function() {
             Catalog( $( this ) );
         });
 
-        $('.aside-menu__wrap').each(function() {
+        $( '.aside-menu__wrap' ).each( function() {
             Menu( $( this ) );
         });
 
-        $('.swiper-container').each(function () {
+        $( '.swiper-container' ).each( function() {
             Slider( $( this ) );
         });
 
-        $('.header__menu-btn').each(function () {
+        $( '.header__menu-btn' ).each( function() {
             MenuOpen($(this));
         });
 
-        $('.aside-menu__btn').each(function () {
+        $( '.aside-menu__btn' ).each( function() {
             MenuOpen( $ ( this ) );
         });
 
-        $('.btn-up').each(function () {
+        $( '.btn-up' ).each( function() {
             SlideToTop( $( this ) );
         });
 
@@ -51,8 +51,8 @@
                         } else {
                             $( _obj ).fadeOut();
                         }
-                        if( $( window ).scrollTop() + $( window ).height() > $( '.site__footer' ).offset().top + 16 ) {
-                            $( _obj ).css( { bottom: $( window ).scrollTop() + $( window ).height() - $( '.footer' ).offset().top + 16 } );
+                        if( $( window ).scrollTop() + $( window ).height() > $( '.site__footer' ).offset().top - 16 ) {
+                            $( _obj ).css( { bottom: $( window ).scrollTop() + $( window ).height() - $( '.site__footer' ).offset().top - 66 } );
                         } else {
                             $( _obj ).css( { bottom: '0' } );
                         }
@@ -233,10 +233,14 @@
         //private properties
         var _self = this,
             _paginator = obj.find($( '.slider__icon' )),
-            _next = $( '.photo-button-next' ),
-            _prev = $( '.photo-button-prev' ),
+            _next = $( '.photo-next' ),
+            _prev = $( '.photo-prev' ),
+            _nextPhoto = $( '.photo_button-next' ),
+            _prevPhoto = $( '.photo_button-prev' ),
             _nextSpecialist = $( '.specialist-button-next' ),
             _prevSpecialist = $( '.specialist-button-prev' ),
+            _nextDescription = $( '.description_button-next' ),
+            _prevDescription = $( '.description_button-prev' ),
             _obj = obj;
 
         //private methods
@@ -267,6 +271,17 @@
             });
 
         }
+        if ( _obj.hasClass( 'photo_wrap' ) ) {
+            var _swiper1 = new Swiper( _obj, {
+                nextButton: _nextPhoto,
+                prevButton: _prevPhoto,
+                slidesPerView: 1,
+                autoplay:5000,
+                speed: 2000,
+                loop: true
+            });
+
+        }
         if ( _obj.hasClass( 'specialist__slider' ) ) {
             var _specialist = new Swiper( _obj, {
                 nextButton: _nextSpecialist,
@@ -278,6 +293,28 @@
                 breakpoints: {
                     940: {
                         slidesPerView: 3
+                    }
+                }
+            });
+        }
+
+        if ( _obj.hasClass( 'description__wrap' ) ) {
+            var _specialist1 = new Swiper( _obj, {
+                nextButton: _nextDescription,
+                prevButton: _prevDescription,
+                slidesPerView: 5,
+                autoplay:5000,
+                speed: 1000,
+                loop: true,
+                breakpoints: {
+                    768: {
+                        slidesPerView: 5
+                    },
+                    600: {
+                        slidesPerView: 3
+                    },
+                    400: {
+                        slidesPerView: 2
                     }
                 }
             });
